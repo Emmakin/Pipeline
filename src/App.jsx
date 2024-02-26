ac0165435a5d3403295d645e5037d9262a56318c
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-02-26 17:49:13 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React, { useRef } from "react";
-import SignUp from "./pages/SignUp";
-import EntryPage from "./pages/EntryPage";
+import SignUp from "./pages/Onboarding/SignUp";
+import EntryPage from "./pages/Onboarding/EntryPage";
+import EmailVeriSent from "./pages/Onboarding/EmailVeriSent";
+import Login from "./pages/Onboarding/Login";
+EmailVeriSent;
 
 function App() {
   return (
-    <div className="wrapper max-w-[475px] mx-auto">
-      <SignUp />
+    <div className="wrapper max-w-[475px] mx-auto bg-paleBlue min-h-screen">
+      {/* <SignUp /> */}
       {/* <EntryPage /> */}
+      {/* <EmailVeriSent /> */}
+      <Login />
     </div>
   );
 }

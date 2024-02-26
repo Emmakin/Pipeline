ca3cd200d6efb8920d3ee5cd662d5d9e873e1a05
Emmakin <101441304+Emmakin@users.noreply.github.com>
2024-02-26 23:05:31 +0100

    Added my modifications for X, Y, and Z features

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import SignUp from "./pages/SignUp";
-import EntryPage from "./pages/EntryPage";
+// import EntryPage from "./pages/EntryPage";
 
 function App() {
   return (
     <div className="wrapper max-w-[475px] mx-auto">
-      {/* <SignUp /> */}
-      <EntryPage />
+      <SignUp />
+      {/* <EntryPage /> */}
     </div>
   );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import LeftArrow from "../assets/chevron-left.svg"
 
 function SignUp() {
-  const thisRef = useRef();
   return (
     <div>
-      <div>header</div>
+      <div>
+        <img src={LeftArrow} alt="left arrow"/>
+        <span>Sign Up</span>
+      </div>
       <section>
         <h3>Welcome to Pipeline</h3>
         <span>complete the sign up to get started</span>
@@ -12,9 +15,12 @@ function SignUp() {
       <form>
         <input placeholder="Name" type="text"></input>
         <input placeholder="Email" type="email"></input>
-        <input placeholder="Phone Number" type="number" ref="thisRef"></input>
+        <input placeholder="Phone Number" type="number"></input>
         <input placeholder="password"></input>
-        <input type="checkbox" />
+        <div>
+          <input type="checkbox" />
+          <span><a>By signing up, you agree to the Terms of Service and Privacy Policy</a></span>
+        </div>
       </form>
     </div>
   );
